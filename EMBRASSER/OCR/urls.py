80da47d676c6d0da67f73676d7408f91dabed052
YOUSBEE <112543451+YOUSBEE@users.noreply.github.com>
2022-12-11 23:10:00 +0900

    변유송#12.11/23:07
    - OCR / urls.py 수정
    - OCR / views.py 수정
    - static / css / member_ocr.css 생성
    - static / js / member_ocr.js 생성
    - template / member_ocr_f.html 생성
    - template / member_ocr_s.html 생성
    - template / member_ocr_t.html 생성

--- EMBRASSER/OCR/urls.py
+++ EMBRASSER/OCR/urls.py
@@ -3,6 +3,8 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('coocr_upload', views.coocr_upload),
-    path('joinmember', views.joinmember)
+    path('coocr_first', views.coocr_first),
+    path('coocr_second', views.coocr_second),
+    path('coocr_third', views.coocr_third),
+    path('join_member', views.join_member),
 ]
