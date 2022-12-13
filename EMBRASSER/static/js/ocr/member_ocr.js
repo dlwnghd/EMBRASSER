3cc0fdfa95e11d20fc4c7c0f540506995861ec2e
dlwnghd <alion0513@naver.com>
2022-12-13 15:22:27 +0900

    이주홍#12.13/15:22
    - 전체 Alert창 디자인 변경

--- EMBRASSER/static/js/ocr/member_ocr.js
+++ EMBRASSER/static/js/ocr/member_ocr.js
@@ -2,6 +2,12 @@ $(document).ready(function(){
     result = $("#warning").text()
     console.log("result :", result)
     if (result == "False") {
-        alert("양식에 맞지 않는 이미지입니다.\n확인 후 다시 넣어주세요!")
+        Swal.fire({
+            icon: "error",
+            title: "OCR 오류",
+            text: "요청한 서류가 아닙니다!"
+        });
     }
 });
+
+console.log("여기에는 왔음")
